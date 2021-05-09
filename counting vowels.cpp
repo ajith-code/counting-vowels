993cead4cdd1ff6356d1bc73610008c12b78b50a
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-09 10:47:54 +0530

    Add files via upload
    To count a vowels in given string

---
+++ counting vowels.cpp
@@ -0,0 +1,46 @@
+
+include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+     
+string a;
+  
+int count=0,c,j;
+  
+char b[5]={'a','e','i','o','u'};
+  
+cout<<"Enter the string";
+  
+getline(cin,a);
+  
+c=a.length();
+  
+for(int i=0;i<=c;i++)
+  
+{
+    
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+      
+{
+          
+count+=1;
+         
+      
+}
+  
+}
+  
+cout<<count<<" vowel character are present in a given string";
+
+  
+
+    
+return 0;
+
+}
